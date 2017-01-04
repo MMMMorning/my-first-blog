813a306365f518e669d1ef9e1625f6b828fa4720
mn <zhangmanning2015@gmail.com>
2017-01-04 23:51:25 +0100

    My Django Girls app, first commit

---
+++ a2room/Scripts/activate.bat
@@ -0,0 +1,32 @@
+@echo off
+set "VIRTUAL_ENV=C:\Users\s141680\TUe\Y2\Q2\web technology\django\djangoassignment\a2room"
+
+if not defined PROMPT (
+    set "PROMPT=$P$G"
+)
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+)
+
+if defined _OLD_VIRTUAL_PYTHONHOME (
+    set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
+)
+
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(a2room) %PROMPT%"
+
+if defined PYTHONHOME (
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+    set PYTHONHOME=
+)
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+) else (
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+)
+
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+
+:END
---
+++ a2room/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\s141680\tue\y2\q2\web technology\django\djangoassignment\a2room\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
